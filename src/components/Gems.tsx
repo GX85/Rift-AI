5b1e2b1a8fcb3761d040a7068106e09b96977482
GX85 <gx85@users.noreply.github.com>
2026-06-24 14:21:02 +0500

    Redraw reference amethyst diamond logo

--- src/components/Gems.tsx
+++ src/components/Gems.tsx
@@ -1,7 +1,7 @@
 export function AmethystLogo({ size = 28 }: { size?: number }) {
   return (
     <span className="amlogo" style={{ width: size, height: size }} aria-hidden>
-      <img src="/amethyst-logo-pixel.svg?v=20260623-logo" alt="" draggable="false" />
+      <img src="/amethyst-logo-pixel.svg?v=20260624-diamond" alt="" draggable="false" />
     </span>
   );
 }
